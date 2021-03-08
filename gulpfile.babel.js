b67435cf1aabd2f2d10c342563dab021ef00dbf8
Hayoung Gam <hahhyoung@gmail.com>
2021-03-08 21:32:40 +0900

    Dev server

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,30 @@
 import gulp from "gulp";
+import gpug from "gulp-pug";
+import del from "del";
+import ws from "gulp-webserver";
+
+const routes = {
+  pug: {
+    src: "src/*.pug",
+    dest: "build"
+  }
+};
+
+const pug = () =>
+  gulp
+    .src(routes.pug.src)
+    .pipe(gpug())
+    .pipe(gulp.dest(routes.pug.dest));
+
+const clean = () => del(["build/"]);
+
+const webserver = () =>
+  gulp.src("build").pipe(ws({ livereload: true, open: true }));
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([pug]);
+
+const postDev = gulp.series([webserver]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
